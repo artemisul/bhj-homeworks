035917aa97cf99b293274b40b4d652b4cf63f36b
Игорь Головатый <i.golovatyy@mangotele.com>
2021-05-18 15:55:57 +0300

    Задача 2 - готово

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -24,6 +24,14 @@ class Game {
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+
+      document.addEventListener('keypress', event => {
+        if (event.key === this.currentSymbol.textContent) {
+          this.success();
+        } else {
+          this.fail();
+        }
+      });
   }
 
   success() {
